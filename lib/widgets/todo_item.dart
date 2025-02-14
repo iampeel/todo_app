fa8db6b93c2f6805537f5cd1620d6e0325e24c1b
Moonpil Cheon <iampeel@naver.com>
2025-02-14 13:23:37 +0900

    6. Todo 완료 상태 관리 구현 - Todo 완료 상태 변경:   - Checkbox 상태 변경 기능   - 완료 시 UI 업데이트   - 저장소 연동
    - 완료 상태 표시 개선:
      - 취소선 스타일 적용
      - 애니메이션 효과 추가
      - 시각적 피드백

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -13,15 +13,36 @@ class TodoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Checkbox(
-        value: todo.isCompleted,
-        onChanged: (_) => onToggle(),
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8,
       ),
-      title: Text(
-        todo.title,
-        style: TextStyle(
-          decoration: todo.isCompleted ? TextDecoration.lineThrough : null,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        decoration: BoxDecoration(
+          color: todo.isCompleted
+              ? Colors.grey.shade100
+              : Colors.white,
+        ),
+        child: ListTile(
+          leading: Checkbox(
+            value: todo.isCompleted,
+            onChanged: (_) => onToggle(),
+          ),
+          title: AnimatedDefaultTextStyle(
+            duration: const Duration(milliseconds: 300),
+            style: TextStyle(
+              fontSize: 16,
+              decoration: todo.isCompleted
+                  ? TextDecoration.lineThrough
+                  : TextDecoration.none,
+              color: todo.isCompleted
+                  ? Colors.grey
+                  : Colors.black,
+            ),
+            child: Text(todo.title),
+          ),
         ),
       ),
     );
